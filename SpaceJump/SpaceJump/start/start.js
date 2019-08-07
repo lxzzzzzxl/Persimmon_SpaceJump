14ca7d6cd5992a2021f3e9978eda729638e4e97a
lxzzzzzxl <1498384737@qq.com>
2019-08-07 09:38:37 +0800

    移植为1024*600版本，全新UI

---
+++ SpaceJump/SpaceJump/start/start.js
@@ -0,0 +1,25 @@
+﻿var page = {
+
+    /* 此方法在第一次显示窗体前发生 */
+    onLoad:function(event){
+
+    },
+
+    /* 此方法展示窗体前发生 */
+    onShow:function(event){
+
+    },
+
+    /* 此方法关闭窗体前发生 */
+    onExit:function(event){
+
+    },
+
+    onBtn:function(event){
+        pm.redirectTo('run/run');
+    },
+};
+
+Page(page);
+
+page = 0;
