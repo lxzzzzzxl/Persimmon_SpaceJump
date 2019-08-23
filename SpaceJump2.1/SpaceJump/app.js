3be775278afb716035c598002a9440a1083bed5f
lxzzzzzxl <1498384737@qq.com>
2019-08-23 10:03:32 +0800

    修复页面跳转BUG

---
+++ SpaceJump2.1/SpaceJump/app.js
@@ -0,0 +1,18 @@
+﻿var app = {
+page : "start/start",
+
+    /* app 加载完成触发该函数 */
+    onLaunch:function(){
+
+    },
+
+    /* app 退出触发该函数 */
+    onExit:function(){
+
+    },
+
+};
+
+App(app);
+
+app = 0;
